3e8cf0e1729420a0929e6bc3c3379f55c01c59ec
Doppon <zaxpkuraudo7777@gmail.com>
2019-04-08 10:11:07 +0900

    英単語のモデルにバリデーションを追加

--- app/models/english_word.rb
+++ app/models/english_word.rb
@@ -1,2 +1,6 @@
 class EnglishWord < ApplicationRecord
+  validates :word, presence: true
+  validates :word_meaning, presence: true
+  validates :sentence, presence: true
+  validates :sentence_meaning, presence: true
 end
